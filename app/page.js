fa8d93216119ba62b3f0ebc9aeeb4d07b85368c7
chaudhuree <chaudhuree00@gmail.com>
2024-08-06 22:00:01 +0600

    made some text changes

--- app/page.js
+++ app/page.js
@@ -4,10 +4,10 @@ const HomePage = () => {
     <div className='hero min-h-screen bg-base-200'>
       <div className='hero-content text-center'>
         <div className='max-w-md'>
-          <h1 className='text-6xl font-bold text-primary'>GPTGenius</h1>
+          <h1 className='text-6xl font-bold text-primary'>Chaudhuree</h1>
           <p className='py-6 text-lg leading-loose'>
-            GPTGenius: Your AI language companion. Powered by OpenAI, it
-            enhances your conversations, content creation, and more!
+            This is a dashboard layout design using clerk auth,
+			daisy ui component library with theme toggle
           </p>
           <Link href='/chat' className='btn btn-secondary '>
             Get Started
